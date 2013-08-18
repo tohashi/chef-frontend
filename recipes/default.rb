83e5c163f83ec8588e0f1f510490b22da2436ea2
t93 <dots64if@gmail.com>
2013-08-19 01:57:02 +0900

    Add grunt-cli installation

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,16 @@
 # Cookbook Name:: chef-frontend
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
+# Copyright (C) 2013 t93
 # 
 # All rights reserved - Do Not Redistribute
 #
+
+bash "install npm packages" do
+  cwd "/home/vagrant"
+  user "root"
+  code <<-EOH
+    npm install -g grunt-cli
+  EOH
+end
+
