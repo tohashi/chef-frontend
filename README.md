619731486eb71af933e91e4a4db12aa0a740da98
t93 <dots64if@gmail.com>
2013-08-18 17:42:38 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# chef-frontend cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: chef-frontend
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
