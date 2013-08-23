18f897636381351ac042d8e5d5c8c241e3b01f73
t93 <dots64if@gmail.com>
2013-08-23 19:17:32 +0900

    Add phantomjs

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,14 @@
 # All rights reserved - Do Not Redistribute
 #
 
+apt_package "libicu48" do
+    action :install
+end
+
+apt_package "phantomjs" do
+    action :install
+end
+
 bash "install npm packages" do
   cwd "/home/vagrant"
   user "root"
